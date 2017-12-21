5af95ae272ecbacea076a253295b7d8869099abf
Peter Eichinger <peter.eichinger@gmail.com>
2017-12-21 10:58:19 +0100

    Add MaxAttribute with associated drawer

---
+++ Editor/src/MaxDrawer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace CustomProperties.Editor {
+
+    /// <summary><see cref="PropertyDrawer"/> for the attribute <see cref="MaxAttribute"/></summary>
+    [CustomPropertyDrawer(typeof(MaxAttribute))]
+    public class MaxDrawer : PropertyDrawer {
+
+        /// <summary>Method that draws the attributed property.</summary>
+        /// <param name="position">Position.</param>
+        /// <param name="property">Property to draw.</param>
+        /// <param name="label">   The label of the property.</param>
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+            if (property.propertyType != SerializedPropertyType.Float) {
+                EditorGuiHelpers.GetLabelContentRects(position, out Rect labelRect, out Rect contentRect);
+                GUI.Label(labelRect, label);
+                EditorGUI.HelpBox(contentRect, "Only applicable to float.", MessageType.Error);
+            }
+
+            float value = EditorGUI.FloatField(position, label, property.floatValue);
+
+            var minAttribute = (MaxAttribute)attribute;
+            value = Mathf.Min(value, minAttribute.Max);
+
+            property.floatValue = value;
+
+            property.serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
---
+++ Runtime/src/MaxAttribute.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace CustomProperties {
+
+    /// <summary>
+    /// Attribute that restricts a field to be always smaller than or equal to a specified value. Only applicable
+    /// to <see cref="float"/> fields.
+    /// </summary>
+    public class MaxAttribute : PropertyAttribute {
+
+        /// <summary>The restricted value.</summary>
+        public float Max { get; set; }
+
+        /// <summary>Create a new instance of this attribute.</summary>
+        /// <param name="max">The value to restrict to.</param>
+        public MaxAttribute(float max) {
+            Max = max;
+        }
+    }
+}
