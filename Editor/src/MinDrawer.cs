cad30972a329c859eddda849ee1a637c8c0ff4f6
Peter Eichinger <peter.eichinger@gmail.com>
2017-12-21 10:51:58 +0100

    Add MinAttribute with associated drawer

---
+++ Editor/src/MinDrawer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace CustomProperties.Editor {
+
+    /// <summary><see cref="PropertyDrawer"/> for the attribute <see cref="MinAttribute"/></summary>
+    [CustomPropertyDrawer(typeof(MinAttribute))]
+    public class MinDrawer : PropertyDrawer {
+
+        /// <summary>Method that draws the attributed property.</summary>
+        /// <param name="position">Position.</param>
+        /// <param name="property">Property to draw.</param>
+        /// <param name="label">   The label of the property.</param>
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+            if (property.propertyType != SerializedPropertyType.Float) {
+                EditorGuiHelpers.GetLabelContentRects(position, out Rect labelRect, out Rect contentRect);
+                GUI.Label(labelRect, label);
+                EditorGUI.HelpBox(contentRect, "Only applicable to float.", MessageType.Error);
+            }
+
+            float value = EditorGUI.FloatField(position, label, property.floatValue);
+
+            var minAttribute = (MinAttribute)attribute;
+            value = Mathf.Max(value, minAttribute.Min);
+
+            property.floatValue = value;
+
+            property.serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
