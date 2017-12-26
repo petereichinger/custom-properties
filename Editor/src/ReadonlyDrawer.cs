6615460dc98e23de070b600efb25f26858a9090b
Peter Eichinger <peter.eichinger@gmail.com>
2017-12-26 02:29:08 +0100

    Add vector2int and vector3 int types to Readonly attribute

--- Editor/src/ReadonlyDrawer.cs
+++ Editor/src/ReadonlyDrawer.cs
@@ -15,6 +15,7 @@ namespace CustomProperties.Editor {
             string value;
 
             switch (property.propertyType) {
+                
                 case SerializedPropertyType.Integer:
                     value = property.intValue.ToString();
                     break;
@@ -42,9 +43,16 @@ namespace CustomProperties.Editor {
                 case SerializedPropertyType.Vector2:
                     value = property.vector2Value.ToString();
                     break;
+                case SerializedPropertyType.Vector2Int:
+                    value = property.vector2IntValue.ToString();
+                    break;
+
 
                 case SerializedPropertyType.Vector3:
                     value = property.vector3Value.ToString();
+                    break;
+                case SerializedPropertyType.Vector3Int:
+                    value = property.vector3IntValue.ToString();
                     break;
 
                 case SerializedPropertyType.Vector4:
@@ -60,7 +68,7 @@ namespace CustomProperties.Editor {
                     break;
 
                 case SerializedPropertyType.Quaternion:
-                    value = string.Format("{0}{1}", property.quaternionValue, property.quaternionValue.eulerAngles);
+                    value = $"{property.quaternionValue}{property.quaternionValue.eulerAngles}";
                     break;
 
                 case SerializedPropertyType.Enum:
