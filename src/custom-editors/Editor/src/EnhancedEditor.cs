76993cdbf1dbc6826023900eb82cdb0c41f6f932
Peter Eichinger <peter.eichinger@gmail.com>
2017-12-31 16:56:37 +0100

    Working first version of editor that shows reorderable list instead of normal arrays

--- src/custom-editors/Editor/src/EnhancedEditor.cs
+++ src/custom-editors/Editor/src/EnhancedEditor.cs
@@ -1,97 +1,71 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEditor;
 using UnityEditorInternal;
 using UnityEngine;
 
 namespace UnityExtensions.CustomEditors {
-    internal static class PropertyExtensions {
-        internal static IEnumerable<SerializedProperty> GetChildren(this SerializedProperty property) {
-            property = property.Copy();
-            var nextElement = property.Copy();
-            bool hasNextElement = nextElement.NextVisible(false);
-            if (!hasNextElement) {
-                nextElement = null;
-            }
-
-            property.NextVisible(true);
-            while (true) {
-                if ((SerializedProperty.EqualContents(property, nextElement))) {
-                    yield break;
-                }
-
-                yield return property;
-
-                bool hasNext = property.NextVisible(false);
-                if (!hasNext) {
-                    break;
-                }
-            }
-        }
-    }
-
     /// <summary>Custom editor with some enhanced functionality.</summary>
     [CanEditMultipleObjects]
     [CustomEditor(typeof(MonoBehaviour), true, isFallback = true)]
     public class EnhancedEditor : Editor {
+        private Dictionary<string, Tuple<ReorderableList, SerializedProperty>> _listDict;
 
-        private Dictionary<string, ReorderableList> _listDict;
-
-   
-
-        void OnEnable() {
-
-            _listDict = new Dictionary<string, ReorderableList>();
+        private void OnEnable() {
+            _listDict = new Dictionary<string, Tuple<ReorderableList, SerializedProperty>>();
             var iterator = serializedObject.GetIterator();
-            
-            for (var enterChildren = true; iterator.NextVisible(enterChildren); enterChildren = false) {
+            iterator.NextVisible(true);
+            do {
                 if (iterator.isArray && iterator.propertyType == SerializedPropertyType.Generic) {
                     var listProperty = serializedObject.FindProperty(iterator.name);
                     var reorderList = new ReorderableList(serializedObject, listProperty, true, true, true, true);
+
                     reorderList.drawElementCallback += (rect, index, active, focused) => { EditorGUI.PropertyField(rect, listProperty.GetArrayElementAtIndex(index)); };
                     reorderList.drawHeaderCallback += rect => GUI.Label(rect, listProperty.displayName);
                     reorderList.onCanRemoveCallback += reorderableList => true;
                     reorderList.onRemoveCallback += reorderableList => { listProperty.DeleteArrayElementAtIndex(reorderableList.index); };
                     reorderList.drawElementCallback += (rect, index, active, focused) => {
-                        var aElement = listProperty.GetArrayElementAtIndex(index);
-                        foreach (var child in aElement.GetChildren()) {
-
-                            float height = EditorGUI.GetPropertyHeight(child);
-                            rect.height = height;
-                            EditorGUI.PropertyField(rect, child, false);
-                            rect.yMin += height;
-                        }
+                        var indRect = new Rect(rect);
+                        var element = listProperty.GetArrayElementAtIndex(index);
+                        EditorGUI.PropertyField(indRect, element, new GUIContent(element.displayName),true);
                     };
                     reorderList.elementHeightCallback += index => {
                         var aElement = listProperty.GetArrayElementAtIndex(index);
-                        float height = 0f;
-                        foreach (var child in aElement.GetChildren()) { 
-
-                            height += EditorGUI.GetPropertyHeight(child);
-                        }
-                        return height;
+                        return EditorGUI.GetPropertyHeight(aElement, true);
                     };
-                    _listDict.Add(listProperty.propertyPath, reorderList);
+                    _listDict.Add(listProperty.propertyPath, Tuple.Create(reorderList, listProperty));
                 }
-            }
+            } while (iterator.NextVisible(false));
         }
-        
+
         /// <summary>Do the inspector gui.</summary>
         public override void OnInspectorGUI() {
             serializedObject.Update();
             var iterator = serializedObject.GetIterator();
-            for (var enterChildren = true; iterator.NextVisible(enterChildren); enterChildren = false) {
-                if (iterator.isArray && iterator.propertyType == SerializedPropertyType.Generic) {
-                    // We have an array and we want to draw a reorderable list.
-                    var list = _listDict[iterator.propertyPath];
-
-                    list.DoLayoutList();
-                } else {
-                    using (new EditorGUI.DisabledScope("m_Script" == iterator.propertyPath)) {
+            iterator.NextVisible(true);
+            do {
+                using (new EditorGUI.DisabledGroupScope(iterator.name == "m_Script")) {
+                    if (iterator.propertyType == SerializedPropertyType.Generic && iterator.isArray) {
+                        var item = _listDict[iterator.propertyPath];
+                        var reorderList = item.Item1;
+                        var listProperty = item.Item2;
+                        reorderList.drawElementCallback = (rect, index, active, focused) => {
+                            var indRect = new Rect(rect);
+                            indRect.x += 8f;
+                            EditorGUI.PropertyField(indRect, listProperty.GetArrayElementAtIndex(index), true);
+                        };
+                        reorderList.elementHeightCallback = index => {
+                            var aElement = listProperty.GetArrayElementAtIndex(index);
+                            return EditorGUI.GetPropertyHeight(aElement, true);
+                        };
+                        EditorGUI.indentLevel++;
+                        reorderList.DoLayoutList();
+                        EditorGUI.indentLevel--;
+                    } else {
                         EditorGUILayout.PropertyField(iterator, true);
                     }
                 }
-
-            }
+            } while (iterator.NextVisible(false));
             serializedObject.ApplyModifiedProperties();
         }
     }
