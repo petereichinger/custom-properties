004acca8c277341bd2c3e0bd04f1642b749e52c1
Peter Eichinger <peter.eichinger@gmail.com>
2018-01-18 16:56:00 +0100

    Add MustBePrefab attribute

---
+++ src/custom-properties/Editor/src/MustBePrefabAttributeDrawer.cs
@@ -0,0 +1,63 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace UnityExtensions.CustomProperties.Editor {
+    [CustomPropertyDrawer(typeof(MustBePrefabAttribute))]
+    internal class MustBePrefabAttributeDrawer : PropertyDrawer {
+
+        private float BoxXOffset => EditorGUIUtility.labelWidth;
+        private float BoxWidth => EditorGUIUtility.fieldWidth;
+        private MustBePrefabAttribute Attribute => (MustBePrefabAttribute) attribute;
+
+        private string GetMessage(SerializedProperty property) {
+            if (property.propertyType != SerializedPropertyType.ObjectReference) {
+                return "Only valid for object references";
+            }
+            return Attribute.Message;
+        }
+
+        private GUIContent GetGUIContent(SerializedProperty property) {
+            var guiContent = new GUIContent(GetMessage(property));
+            return guiContent;
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label) {
+            var height = base.GetPropertyHeight(property, label);
+            if (!IsPrefab(property)) {
+                height += EditorStyles.helpBox.CalcHeight(GetGUIContent(property), BoxWidth);
+            }
+            return height;
+        }
+        
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+            var height = base.GetPropertyHeight(property, label);
+
+            position.height = height;
+            EditorGUI.PropertyField(position, property, label);
+            position.yMin += height;
+          
+            if (!IsPrefab(property)) {
+                var size = EditorStyles.helpBox.CalcHeight(GetGUIContent(property), BoxWidth);
+                var boxRect = new Rect(position) {height = size};
+                boxRect.xMin += BoxXOffset;
+                EditorGUI.HelpBox(boxRect,GetMessage(property),MessageType.Error);
+                position.yMin += size;
+            }
+
+        }
+
+
+        private bool IsPrefab(SerializedProperty property) {
+            if (property.propertyType != SerializedPropertyType.ObjectReference) {
+                return false;
+            }
+            if (property.objectReferenceValue == null) {
+                return false;
+            }
+
+            var prefabType = PrefabUtility.GetPrefabType(property.objectReferenceValue);
+
+            return prefabType == PrefabType.Prefab || prefabType == PrefabType.ModelPrefab;
+        }
+    }
+}
---
+++ src/custom-properties/Runtime/src/MustBePrefabAttribute.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace UnityExtensions.CustomProperties {
+    /// <summary>
+    /// Attribute that shows a message when the attached property is not a prefab.
+    /// </summary>
+    public class MustBePrefabAttribute : PropertyAttribute {
+        /// <summary>
+        /// Message to show.
+        /// </summary>
+        public string Message { get; set; }
+        /// <summary>
+        /// Create a new instance of this attribute.
+        /// </summary>
+        /// <param name="message">Message to show.</param>
+        public MustBePrefabAttribute(string message = "Please select a prefab.") {
+            Message = message;
+        }
+    }
+}
