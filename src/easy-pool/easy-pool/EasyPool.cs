b325f6d9d60dff9151698acaaba6109bd4fd8c0a
Peter Eichinger <peter.eichinger@gmail.com>
2018-07-06 11:53:20 +0200

    Add simple object pool project

---
+++ src/easy-pool/easy-pool/EasyPool.cs
@@ -0,0 +1,61 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using ID = System.Int32;
+
+namespace UnityExtensions.EasyPool {
+	public class EasyPool : MonoBehaviour, ISerializationCallbackReceiver {
+
+		private static EasyPool _instance;
+		public static EasyPool Instance {
+			get {
+				if (!_instance) {
+					_instance = FindObjectOfType<EasyPool>();
+					Assert.IsNotNull(_instance, "No EasyPool in scene");
+				}
+
+				return _instance;
+			}
+		}
+
+		private Dictionary<ID, Pool> _poolDict= new Dictionary<int, Pool>();
+
+		[SerializeField, HideInInspector]
+		private List<Pool> _poolSerialization;
+		public void OnBeforeSerialize() {
+			if (_poolDict != null) {
+				_poolSerialization = new List<Pool>(_poolDict.Values);
+			}
+		}
+
+		public void OnAfterDeserialize() {
+			_poolDict = new Dictionary<int, Pool>();
+			foreach (var pool in _poolSerialization) {
+				_poolDict[pool.instanceID] = pool;
+			}
+		}
+
+		public GameObject Spawn(GameObject prefab, Vector3 position, Quaternion rotation, Transform parent = null) {
+			if (!_poolDict.ContainsKey(prefab.GetInstanceID())) {
+				_poolDict[prefab.GetInstanceID()] = Pool.CreatePool(prefab, transform);
+			}
+
+			return _poolDict[prefab.GetInstanceID()].Spawn(position, rotation, parent);
+		}
+
+		public void Despawn(GameObject objectToDespawn) {
+			int id = objectToDespawn.GetInstanceID();
+			Pool pool = null;
+			foreach (var poolEntry in _poolDict) {
+				if (poolEntry.Value.IsMember(id)) {
+					pool = poolEntry.Value;
+				}
+			}
+
+			Assert.IsNotNull(pool, "Object that is not pooled was despawned");
+
+			pool.Despawn(objectToDespawn);
+		}
+	}
+}
---
+++ src/easy-pool/easy-pool/Pool.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+using ID = System.Int32;
+
+namespace UnityExtensions.EasyPool {
+
+	[Serializable]
+	public class Pool : ScriptableObject, ISerializationCallbackReceiver {
+		public ID instanceID;
+		public GameObject prefab;
+
+		private Transform _inactiveContainer;
+
+		private Stack<GameObject> _inactive;
+		private HashSet<ID> _members;
+
+		private List<ID> _membersSerializer;
+		private List<GameObject> _inactiveSerializer;
+
+		public static Pool CreatePool(GameObject poolObjectPrefab, Transform inactiveContainer) {
+			var instance = CreateInstance<Pool>();
+
+			instance.instanceID = poolObjectPrefab.GetInstanceID();
+			instance.prefab = poolObjectPrefab;
+			instance._inactive = new Stack<GameObject>();
+			instance._members = new HashSet<int>();
+			instance._inactiveContainer = inactiveContainer;
+
+			return instance;
+		}
+
+		public void OnBeforeSerialize() {
+			_inactiveSerializer = _inactive != null ? new List<GameObject>(_inactive) : new List<GameObject>();
+			_membersSerializer = _members != null ? new List<ID>(_members) : new List<ID>();
+		}
+
+		public void OnAfterDeserialize() {
+			_inactive = _inactiveSerializer != null
+				? new Stack<GameObject>(_inactiveSerializer)
+				: new Stack<GameObject>();
+
+			_members =_membersSerializer!=null ? new HashSet<int>(_membersSerializer) : new HashSet<int>();
+		}
+
+
+		public GameObject Spawn(Vector3 position, Quaternion rotation, Transform parent = null) {
+			GameObject instance;
+
+			if (_inactive.Count == 0) {
+				instance = Instantiate(prefab, parent, false);
+				_members.Add(instance.GetInstanceID());
+			} else {
+				instance = _inactive.Pop();
+				if (instance == null) {
+					return Spawn(position, rotation);
+				}
+			}
+
+			instance.transform.SetParent(parent, false);
+			instance.transform.localPosition = position;
+			instance.transform.localRotation = rotation;
+			instance.SetActive(true);
+			return instance;
+		}
+
+		public void Despawn(GameObject objectToDespawn) {
+			objectToDespawn.SetActive(false);
+
+			objectToDespawn.transform.SetParent(_inactiveContainer ,false);
+			_inactive.Push(objectToDespawn);
+		}
+
+		public bool IsMember(GameObject instance) {
+			return _members.Contains(instance.GetInstanceID());
+		}
+
+		public bool IsMember(ID id) {
+			return _members.Contains(id);
+		}
+	}
+}
